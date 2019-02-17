5c3e8540b28bc50e28d2c25b71870948eb6d8e59
ll9 <llwierer@web.de>
2019-02-17 14:51:23 +0100

    Cef Multiple Browsers

--- CefMultipleWindows/Form1.cs
+++ CefMultipleWindows/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CefSharp;
+using CefSharp.WinForms;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +17,32 @@ namespace CefMultipleWindows
         public Form1()
         {
             InitializeComponent();
+
+            InitializeChromium();
+        }
+
+        public void InitializeChromium()
+        {
+            CefSettings settings = new CefSettings();
+            // Initialize cef with the provided settings
+            Cef.Initialize(settings);
+
+            var browser = new ChromiumWebBrowser("https://stackoverflow.com/questions/39032447/load-2-urls-at-the-same-time-in-different-instances-cefsharp");
+            browser.Dock = DockStyle.Left;
+            browser.Height = 400;
+            browser.Width = 400;
+            Controls.Add(browser);
+
+            var browser2 = new ChromiumWebBrowser("https://github.com/cefsharp/CefSharp.MinimalExample/blob/master/CefSharp.MinimalExample.WinForms/Program.cs#L16");
+            browser2.Dock = DockStyle.Left;
+            browser2.Height = 400;
+            browser2.Width = 400;
+            Controls.Add(browser2);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Cef.Shutdown();
         }
     }
 }
